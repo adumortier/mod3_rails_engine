eb1cfcca8d7620f26296384ebf433c9a6441614c
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-18 18:23:40 -0600

    Add invoice and invoice_items tests and models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,11 @@
 class Invoice < ApplicationRecord
   belongs_to :merchant
   belongs_to :customer
+  has_many :transactions
 
   validates_presence_of :status
+
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 
 end
