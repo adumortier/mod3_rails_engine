14fc4b71a01d526d1c46e4e2bf848aa8c08dfe9a
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-19 17:51:35 -0600

    Add action methods for each resource

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::ItemsController < ApplicationController 
+
+  def index
+    if params[:merchant_id]
+      render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+    else
+      render json: ItemSerializer.new(Item.all)
+    end
+  end
+
+  def show 
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create 
+    merchant = Merchant.find(item_params[:merchant_id])
+    render json: ItemSerializer.new(merchant.items.create(item_params))
+  end
+
+  def update 
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy 
+    item = Item.find(params[:id])
+    Item.delete(params[:id])
+    render json: ItemSerializer.new(item)
+  end
+
+  private 
+
+  def item_params 
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+end
