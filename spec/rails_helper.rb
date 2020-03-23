306d482a44c3e693f9768e494a4f15d672cc3863
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-22 22:59:47 -0600

    Add simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -61,7 +64,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-    Shoulda::Matchers.configure do |config|
+  Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
 
