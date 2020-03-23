95482b9c21614936d08c39199d4df25135f91f7f
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-22 21:45:54 -0600

    Add routes for business end points

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
-        get '/most_revenue', to: 'business#revenue'
+        get '/most_revenue', to: 'business#most_revenue'
+        get '/most_items', to: 'business#most_items'
       end
+
+      get '/revenue', to: 'merchants/business#revenue'
 
       namespace :items do
         get '/find', to: 'search#show'
@@ -16,12 +19,12 @@ Rails.application.routes.draw do
 
       resources :merchants, except: [:new, :edit] do 
         resources :items, only: [:index]
+        get '/revenue', to: 'merchants/business#merchant_revenue'
       end
 
       resources :items, except: [:new, :edit] 
 
       get '/items/:item_id/merchant', to: 'merchants#show'
-      
         
     end
   end
