91ffac0711f880e76dfccbbcdd4afc7a19f8159e
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-03-22 22:58:02 -0600

    Remove validation of presence

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
 
+  # validates :credit_card_expiration_date, presence: true
   validates_presence_of :credit_card_number
-  validates_presence_of :credit_card_expiration_date
+  # validates_presence_of :credit_card_expiration_date
   validates_presence_of :result
 end
